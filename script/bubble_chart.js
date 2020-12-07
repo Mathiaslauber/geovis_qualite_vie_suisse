08097e2dca66ec0586dfc5fab4b3fb70c899ebc2
Mathiaslauber <37750575+Mathiaslauber@users.noreply.github.com>
2020-12-07 10:34:41 +0100

    intégration du scatter plot motorisation

--- script/bubble_chart.js
+++ script/bubble_chart.js
@@ -6,12 +6,12 @@
 // [] Problème de couleur pour certaines villes 
 
 // set the dimensions and margins of the graph
-var margin = { top: 65, right: 20, bottom: 50, left: 45 },
+var margin = { top: 30, right: 20, bottom: 50, left: 45 },
     width = 646 - margin.left - margin.right,
-    height = 350 - margin.top - margin.bottom;
+    height = 380 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
-var svg_graph1 = d3.select("#graph1")
+var svg_graph1 = d3.select("#descriptif1")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -26,16 +26,16 @@ d3.csv("data/mobi_final_12_18.csv", function(data) {
     //     TITLE & AXIS & SCALE   //
     // ---------------------------//
     svg_graph1.append("text")
-    .attr("x", (width / 2))
-    .attr("y", 15 - (margin.top / 2))
-    .attr("text-anchor", "middle")
-    .style("font-size", "16px")
-    .style("text-decoration", "bold")
-    .text("Villes suisses et choix de mobilité");
+        .attr("x", margin.left + 100)
+        .attr("y", 15 - (margin.top / 2))
+        .attr("text-anchor", "middle")
+        .style("font-size", "16px")
+        .style("text-decoration", "bold")
+        .text("Villes suisses et choix de mobilité");
     // Add X axis
     var x = d3.scaleLinear()
         .domain([10, 70])
-        .range([0, width-(width/5)]);
+        .range([0, width - (width / 5)]);
     svg_graph1.append("g")
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x).ticks(4));
@@ -43,7 +43,7 @@ d3.csv("data/mobi_final_12_18.csv", function(data) {
     // Add X axis label:
     svg_graph1.append("text")
         .attr("text-anchor", "end")
-        .attr("x", width)
+        .attr("x", width - 100)
         .attr("y", height + 30)
         .text("[%]Part des pendulaires en transport public")
         //.attr("text-anchor", "right")
@@ -54,22 +54,22 @@ d3.csv("data/mobi_final_12_18.csv", function(data) {
     // Add Y axis
     var y = d3.scaleLinear()
         .domain([10, 70])
-        .range([height, 0]);
+        .range([height, 0 + (height / 7)]);
     svg_graph1.append("g")
         .call(d3.axisLeft(y));
 
     // Add Y axis label:
     svg_graph1.append("text")
         .attr("text-anchor", "end")
-        .attr("x", - 210)
-        .attr("y", - 30)
+        .attr("x", -250)
+        .attr("y", -30)
         .text("[%]Part des pendulaires en auto")
         .attr("text-anchor", "start")
-         //.attr("text-anchor", "right")
-         .attr("transform", "rotate(-90)")
-         .style("font-size", "14px")
-         .style("text-decoration", "italic")
-         .style("letter-spacing", "-0.75px");
+        //.attr("text-anchor", "right")
+        .attr("transform", "rotate(-90)")
+        .style("font-size", "14px")
+        .style("text-decoration", "italic")
+        .style("letter-spacing", "-0.75px");
 
     // Add a scale for bubble size
     var z = d3.scaleSqrt()
@@ -77,18 +77,18 @@ d3.csv("data/mobi_final_12_18.csv", function(data) {
         .range([2, 30]);
 
     // Add a scale for bubble color
-    
+
     //mygroup = ["Zurich", "Berne", "Lucerne", "Bale", "St-Gall", "Lugano", "Lausanne", "Geneve"]
     mygroup = ["Zurich_ville", "Zurich_agglo", "Berne_ville", "Berne_agglo", "Lucerne_ville", "Lucerne_agglo", "Bale_ville", "Bale_agglo", "St-Gall_ville", "St-Gall_agglo", "Lugano_ville", "Lugano_agglo", "Lausanne_ville", "Lausanne_agglo", "Geneve_ville", "Geneve_agglo"]
- var myColor2 = d3.scaleOrdinal()
+    var myColor2 = d3.scaleOrdinal()
         .domain(mygroup)
-        .range(["#457b9d","#457b9d", "#e63946", "#e63946", "#48cae4", "#48cae4", "#2b2d42", "#2b2d42", "#006d77", "#006d77", "#b56576", "#b56576", "#52b788",  "#52b788", "#ffcb77","#ffcb77"]);
-    
+        .range(["#457b9d", "#457b9d", "#e63946", "#e63946", "#48cae4", "#48cae4", "#2b2d42", "#2b2d42", "#006d77", "#006d77", "#b56576", "#b56576", "#52b788", "#52b788", "#ffcb77", "#ffcb77"]);
+
     var myColor = d3.scaleOrdinal()
         .domain(["Zurich", "Berne", "Lucerne", "Bale", "St-Gall", "Lugano", "Lausanne", "Geneve"])
         //.domain(mygroup)
         .range(["#457b9d", "#e63946", "#48cae4", "#2b2d42", "#006d77", "#b56576", "#52b788", "#ffcb77"]);
-// zurich originelle couleur "#457b9d"
+    // zurich originelle couleur "#457b9d"
     var mycolor_agglo = d3.scaleOrdinal()
         .domain(["Ville-centre", "Agglomeration"])
         .range(["#000", "#7A7A7A"]);
@@ -99,7 +99,7 @@ d3.csv("data/mobi_final_12_18.csv", function(data) {
     // regarder overstack flow pour le problème de tooltip 
 
     // -1- Create a tooltip div that is hidden by default:
-    var tooltip = d3.select("#graph1")
+    var tooltip = d3.select("#descriptif1")
         .append("div")
         .style("opacity", 0)
         .attr("width", "100px")
@@ -111,7 +111,7 @@ d3.csv("data/mobi_final_12_18.csv", function(data) {
         .style("color", "white")
 
     // -2- Create 3 functions to show / update (when mouse move but stay on same circle) / hide the tooltip
-   /*
+    /*
    VERSION OBSOLETE DU TOOLTIP
    var showTooltip = function(d) {
         tooltip
@@ -141,19 +141,19 @@ d3.csv("data/mobi_final_12_18.csv", function(data) {
 
     var showTooltip = function(d) {
         tooltip
-        .transition()
-        .duration(200)
-    
+            .transition()
+            .duration(200)
+
         tooltip
             .style("opacity", 1)
-            .html("concerne : " + d.villes + " et " + d.nbr_auto_1000hab + " automobiles") 
+            .html("concerne : " + d.villes + " et " + d.nbr_auto_1000hab + " automobiles")
             .style("left", (d3.mouse(this)[0] + 70) + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
-            .style("top", (d3.mouse(this)[1]) + "px") 
+            .style("top", (d3.mouse(this)[1]) + "px")
     }
 
     var moveTooltip = function(d) {
-        tooltip        
-            .html("concerne : " + d.villes + " et " + d.nbr_auto_1000hab + " automobiles") 
+        tooltip
+            .html("concerne : " + d.villes + " et " + d.nbr_auto_1000hab + " automobiles")
             .style("left", (d3.mouse(this)[0] + 70) + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
             .style("top", (d3.mouse(this)[1]) + "px")
     }
@@ -182,11 +182,11 @@ d3.csv("data/mobi_final_12_18.csv", function(data) {
 
     // And when it is not hovered anymore
     var noHighlight = function(d) {
-        d3.selectAll(".bubbles2").style("opacity", 1)
-    }
- // ---------------------------//
-    //       HIGHLIGHT GROUP      //
-    // ---------------------------//
+            d3.selectAll(".bubbles2").style("opacity", 1)
+        }
+        // ---------------------------//
+        //       HIGHLIGHT GROUP      //
+        // ---------------------------//
 
     // What to do when one group is hovered
     var highlight1 = function(d) {
@@ -232,7 +232,7 @@ d3.csv("data/mobi_final_12_18.csv", function(data) {
     var valuesToShow = [350, 450, 650]
     var xCircle = 80
     var xLabel = 120
-    var yCircle = 305
+    var yCircle = 390
 
     svg_graph1
         .selectAll("legend")
@@ -272,7 +272,7 @@ d3.csv("data/mobi_final_12_18.csv", function(data) {
 
     // Legend title
     svg_graph1.append("text")
-        .attr('x', xCircle -70)
+        .attr('x', xCircle - 70)
         .attr("y", yCircle - 100 + 20)
         .text("Nombre d'autos pour 1000 habitants)")
         .attr("text-anchor", "left")
@@ -298,7 +298,7 @@ d3.csv("data/mobi_final_12_18.csv", function(data) {
         .style("fill", function(d) { return myColor(d) })
         .on("mouseover", highlight1)
         .on("mouseleave", noHighlight1);
-        
+
 
 
     // Add labels beside legend dots
@@ -309,8 +309,8 @@ d3.csv("data/mobi_final_12_18.csv", function(data) {
         .attr("x", 475 + size * .8)
         .attr("y", function(d, i) { return i * (size + 5) + (size / 2) }) // 100 is where the first dot appears. 25 is the distance between dots
         .style("fill", function(d) { return myColor(d) })
-        .style("stroke","black")
-        .style("stroke-width","0.09")
+        .style("stroke", "black")
+        .style("stroke-width", "0.09")
         .text(function(d) { return d })
         .attr("text-anchor", "left")
         .style("alignment-baseline", "middle")
@@ -319,7 +319,7 @@ d3.csv("data/mobi_final_12_18.csv", function(data) {
         .style("letter-spacing", "-0.75px")
         .on("mouseover", highlight1)
         .on("mouseleave", noHighlight1);
-        
+
 
 
 
